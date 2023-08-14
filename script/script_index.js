0356072e98e3c7000123ae4bd72d4c318590a320
abhinavramesh29 <139799143+abhinavramesh29@users.noreply.github.com>
2023-08-14 09:02:29 +0800

    Add files via upload

---
+++ script/script_index.js
@@ -0,0 +1,63 @@
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+
+}
+
+// Side NavIgation Menu JS Code
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Student", "Aspiring Cybersecurity professional", "Avid Learner", "Theatre Graduate", "Actor"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+
+// Side Navigation Bar Close While We Click On Navigation Links
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  });
+}
+
+function showMessage() {
+  alert('Thank you for contacting me! Your message has been received and I will get back to you as soon as possible.');
+  
+  // Reset the form fields
+  document.getElementById("contactForm").reset();
+  
+  return false; // This will prevent the form from actually submitting and refreshing the page
+}
+
